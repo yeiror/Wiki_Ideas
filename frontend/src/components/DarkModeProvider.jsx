2c397f1c7cf40b5543b15395461163e47da7215d
Yair Riascos <yeiror@gmail.com>
2023-02-02 21:36:18 -0500

    Frontend folder added to main branch (#4)
    * initial homepage
    
    * Testing Frontend Configuration, Fixed ReadMe dentro de Backend
    
    ---------
    
    Co-authored-by: LailaJalil <lailajalil@gmail.com>

---
+++ frontend/src/components/DarkModeProvider.jsx
@@ -0,0 +1,20 @@
+import React, {createContext, useState} from 'react';
+import '../styles/styles.css'
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider(props){
+    const [darkMode, setDarkMode] = useState(true);
+    const toggleDarkMode = (dark) => {
+        setDarkMode(!darkMode);
+    };
+    return (
+        <div>  
+            <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
+                {props.children}
+            </DarkModeContext.Provider>
+        </div>
+    )
+};
+
+export {DarkModeContext, DarkModeProvider};
